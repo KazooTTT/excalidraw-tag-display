f6a547c4ccaef4947ba2e7b968cdfdd87285cc08
KazooTTT <work@kazoottt.top>
2025-03-27 00:45:29 +0800

    feat: 为 Excalidraw 文件添加标签显示
    - 在文件资源管理器中，为 .excalidraw.md 文件添加 "excalidraw" 标签，以便用户快速识别。

--- main.ts
+++ main.ts
@@ -1,134 +1,125 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import { Plugin } from "obsidian";
 
-// Remember to rename these classes and interfaces!
+class Logger {
+	private pluginName: string;
 
-interface MyPluginSettings {
-	mySetting: string;
-}
+	constructor(pluginName: string) {
+		this.pluginName = pluginName;
+	}
 
-const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
+	log(...args: any[]) {
+		console.log(`[${this.pluginName}]`, ...args);
+	}
+
+	error(...args: any[]) {
+		console.error(`[${this.pluginName}]`, ...args);
+	}
 }
 
 export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
+	private logger: Logger;
 
-	async onload() {
-		await this.loadSettings();
+	constructor(app: any, manifest: any) {
+		super(app, manifest);
+		this.logger = new Logger("Excalidraw Tag Display");
+	}
 
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
+	addExcalidrawTags = () => {
+		this.logger.log("Starting addExcalidrawTags");
+		const fileExplorers =
+			this.app.workspace.getLeavesOfType("file-explorer");
+		this.logger.log("Found file explorers:", fileExplorers.length);
 
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
+		fileExplorers.forEach((fileExplorer) => {
+			this.logger.log("File explorer view:", fileExplorer.view);
+			const fileItems = (fileExplorer.view as any).fileItems;
+			this.logger.log("Raw fileItems:", fileItems);
+			this.logger.log("File items keys:", Object.keys(fileItems).length);
 
-		// This adds a simple command that can be triggered anywhere
-		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
-			}
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
+			for (const path in fileItems) {
+				const fileItem = fileItems[path];
+				if (path.endsWith(".excalidraw.md")) {
+					this.logger.log("Found excalidraw file:", path);
+					this.logger.log("Full fileItem:", fileItem);
+
+					if (!fileItem || !fileItem.selfEl) {
+						this.logger.error(
+							"fileItem or fileItem.selfEl is missing for path:",
+							path
+						);
+						continue;
 					}
 
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
+					// 检查是否已经添加了标签
+					const existingTag =
+						fileItem.selfEl.querySelector(".nav-file-tag");
+					if (existingTag) {
+						this.logger.log("Tag already exists for:", path);
+						continue;
+					}
+
+					try {
+						const tag = createDiv({
+							cls: "nav-file-tag",
+							text: "excalidraw",
+						});
+
+						// 获取正确的目标元素
+						const selfEl = fileItem.selfEl;
+						if (selfEl) {
+							selfEl.appendChild(tag);
+							this.logger.log("Successfully added tag to:", path);
+						} else {
+							this.logger.error(
+								"Could not find tree-item-inner for:",
+								path
+							);
+						}
+					} catch (error) {
+						this.logger.error("Error adding tag to", path, error);
+					}
 				}
 			}
 		});
+	};
 
-		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
+	async onload() {
+		this.logger.log("Plugin loading");
+		await this.loadSettings();
+		this.logger.log("Settings loaded");
 
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
+		// Wait for workspace to be ready
+		this.app.workspace.onLayoutReady(() => {
+			this.logger.log("Workspace layout is ready");
+			this.addExcalidrawTags();
+
+			// 监听文件资源管理器的变化
+			this.registerEvent(
+				this.app.workspace.on("file-menu", () => {
+					this.logger.log("File menu event triggered");
+					this.addExcalidrawTags();
+				})
+			);
+
+			// 监听文件变化
+			this.registerEvent(
+				this.app.vault.on("create", () => {
+					this.logger.log("File created event triggered");
+					this.addExcalidrawTags();
+				})
+			);
 		});
-
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
 	}
 
 	onunload() {
-
+		this.logger.log("Plugin unloading");
 	}
 
 	async loadSettings() {
-		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
+		this.logger.log("Loading settings");
 	}
 
 	async saveSettings() {
-		await this.saveData(this.settings);
-	}
-}
-
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
-	}
-
-	onOpen() {
-		const {contentEl} = this;
-		contentEl.setText('Woah!');
-	}
-
-	onClose() {
-		const {contentEl} = this;
-		contentEl.empty();
-	}
-}
-
-class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
-
-	constructor(app: App, plugin: MyPlugin) {
-		super(app, plugin);
-		this.plugin = plugin;
-	}
-
-	display(): void {
-		const {containerEl} = this;
-
-		containerEl.empty();
-
-		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
-			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
-				.onChange(async (value) => {
-					this.plugin.settings.mySetting = value;
-					await this.plugin.saveSettings();
-				}));
+		this.logger.log("Saving settings");
 	}
 }
